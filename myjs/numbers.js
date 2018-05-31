5d2f3ab8f653ffaf0b3b06870d9882fdb582689d
ytyc2k <ytyc2k@gmail.com>
2018-05-31 06:23:19 -0500

    Initial commit

---
+++ myjs/numbers.js
@@ -0,0 +1,7 @@
+var i=0;
+for(i=0;i<=11;i++)
+{if (i==3)
+{continue;}
+document.write("The number is " + i);
+document.write("<br/>");
+}
---
+++ numbers.js
@@ -0,0 +1,8 @@
+var i=0;
+for(i=0;i<=11;i++)
+{
+    if (i==3)
+        {document.write("The number is " + i);}
+    document.write("The number is " + i);
+    document.write("<br/>");
+}
